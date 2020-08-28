eceb76cd90cfe440c8bc2753c115fd19df52fe4f
rpotter12 <rohitpotter12@gmail.com>
2020-08-29 01:45:05 +0530

    React app intial files

---
+++ React app files/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Home from "./pages/Home";
+import Search from "./pages/Search"
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+function App() {
+  return (
+        <div className="app">
+          <Router>
+            <Switch>
+              <Route path="/search">
+                <Search />
+              </Route>
+
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+    );
+}
+
+
+export default App;
---
+++ React app files/components/Command.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Command extends React.Component {
+    render() { 
+        return ( 
+            <React.Fragment>
+                <h1>Hello world</h1>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Command;
